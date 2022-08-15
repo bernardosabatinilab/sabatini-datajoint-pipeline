2f1e399c95811733cb9164b17f4943c0563a8600
JR <jaerong.ahn@datajoint.com>
2022-08-15 15:50:36 -0500

    revise paths.py

--- workflow/utils/paths.py
+++ workflow/utils/paths.py
@@ -2,33 +2,45 @@ import datajoint as dj
 import pathlib
 
 
-def get_ephys_root_data_dir():
-    data_dir = dj.config.get('custom', {}).get('ephys_root_data_dir', None)
+def get_raw_root_data_dir():
+    data_dir = dj.config.get("custom", {}).get("root_data_dir", None)
     return pathlib.Path(data_dir) if data_dir else None
+
+
+def get_processed_root_data_dir():
+    data_dir = dj.config.get("custom", {}).get("processed_data_dir", None)
+    return pathlib.Path(data_dir) if data_dir else None
+
+
+# for element-array-ephys activation
+def get_ephys_root_data_dir():
+    return get_raw_root_data_dir()
 
 
 def get_ephys_processed_root_data_dir():
-    data_dir = dj.config.get('custom', {}).get('ephys_processed_data_dir', None)
-    return pathlib.Path(data_dir) if data_dir else None
+    return get_processed_root_data_dir()
 
 
 def get_ephys_session_directory(session_key: dict) -> str:
     data_dir = get_ephys_root_data_dir()
 
     from workflow.pipeline import session
-    if not (session.SessionDirectory & session_key):
-        raise FileNotFoundError(f'No session data directory defined for {session_key}')
 
-    sess_dir = data_dir / (session.SessionDirectory & session_key).fetch1('session_dir')
+    if not (session.SessionDirectory & session_key):
+        raise FileNotFoundError(f"No session data directory defined for {session_key}")
+
+    sess_dir = data_dir / (session.SessionDirectory & session_key).fetch1("session_dir")
 
     return sess_dir.as_posix()
 
+
+# for element-calcium-imaging activation
 def get_imaging_root_data_dir():
-    return get_ephys_root_data_dir() 
+    return get_raw_root_data_dir()
 
 
 def get_imaging_processed_root_data_dir():
-    return get_ephys_processed_root_data_dir()
+    return get_processed_root_data_dir()
 
 
 def get_scan_image_files(scan_key):
@@ -49,8 +61,10 @@ def get_scan_image_files(scan_key):
         raise FileNotFoundError(f"No tiff file found in {sess_dir}")
 
 
+# for element-deeplabcut activation
 def get_dlc_root_data_dir():
-    return get_imaging_root_data_dir()
+    return get_raw_root_data_dir()
+
 
 def get_dlc_processed_data_dir():
-    return get_imaging_processed_root_data_dir()
+    return get_processed_root_data_dir()
