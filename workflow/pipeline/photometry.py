d1b2b7a64428c5d2b798c632d4715eaf9ecc58b5
JR <jaerong.ahn@datajoint.com>
2022-08-17 16:30:53 -0500

    ✨ add make method in photometry.py

--- workflow/pipeline/photometry.py
+++ workflow/pipeline/photometry.py
@@ -2,9 +2,14 @@ import datajoint as dj
 import numpy as np
 from .core import session
 from . import get_schema_name
+from workflow import db_prefix
+from element_interface.utils import find_full_path
+from workflow.utils.paths import get_raw_root_data_dir
+import scipy.io as spio
+from pathlib import Path
 
 logger = dj.logger
-schema = dj.schema(get_schema_name("photometry"))
+schema = dj.schema(db_prefix + "photometry")
 
 
 @schema
@@ -31,8 +36,8 @@ class EmissionColor(dj.Lookup):
 class FiberPhotometry(dj.Imported):
     definition = """
     -> session.Session
-    fiber_id: in Id of the implanted fibers
-    ---t  #
+    fiber_id: int  # id of the implanted fibers
+    ---
     sample_rate: float (Hz) 
     notes='': varchar(1000)  
     """
@@ -55,15 +60,41 @@ class FiberPhotometry(dj.Imported):
         time_offset: float  # (in second) time offset to synchronize the photometry traces to the master clock
         """
 
-    def make():
-        # go to session_dir / photometry
-        # retrieves all photometry data - determine the number of fibers
-        # run through the preprocessing code - for each fiber - should return
-        #   - fiber_id
-        #   - channels
-        #       - excitation wavelength
-        #       - emission color
-        #       - preprocessed traces
-        #       - preprocessed timestamps (timeoffset applied)
+    def make(self, key):
 
-        pass
+        sample_rate = 50
+        time_offset = 67.94
+        excitation_wavelength = 405
+        emission_color = "green"
+
+        session_dir = (session.SessionDirectory & key).fetch1("session_dir")
+        photometry_dir = (
+            find_full_path(get_raw_root_data_dir(), session_dir) / "photometry"
+        )
+
+        for fiber in photometry_dir.iterdir:
+
+            fiber_id = fiber.stem
+
+            self.insert1([*key.values(), fiber_id, sample_rate])
+
+            for channel in photometry_dir.rglob("*.csv"):
+
+                channel_id = channel.stem[-1]
+                trace = spio.loadmat(channel, simplify_cells=True)[f"ch{channel_id}"]
+                duration = len(trace) / sample_rate
+                timestamps = np.arange(
+                    time_offset, duration, (1 / sample_rate), dtype=np.float32
+                )
+                self.Trace.insert1(
+                    [
+                        *key.values(),
+                        channel_id,
+                        excitation_wavelength,
+                        emission_color,
+                        timestamps,
+                        trace,
+                    ]
+                )
+
+        self.TimeOffset.insert1([*key.values(), time_offset])
