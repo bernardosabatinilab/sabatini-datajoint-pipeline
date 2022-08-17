96853a67bd25b28fd94e569c1317c60b20c9c0b7
Ubuntu <ubuntu@ip-172-20-48-232.us-east-2.compute.internal>
2022-08-17 16:31:55 +0000

    reorganize BrainRegion schema to inherit from CCF

--- workflow/pipeline/imaging.py
+++ workflow/pipeline/imaging.py
@@ -18,9 +18,28 @@ Session = session.Session
 Equipment = lab.Equipment
 
 @lab.schema
+class CCF(dj.Lookup):
+    definition = """  # Common Coordinate Framework
+    ccf_id            : int             # CCF ID, a.k.a atlas ID
+    ---
+    ccf_version       : varchar(64)     # Allen CCF Version - e.g. CCFv3
+    ccf_resolution    : float           # voxel resolution in micron
+    ccf_description='': varchar(255)    # CCFLabel Description
+    """
+
+    class Voxel(dj.Part):
+        definition = """  # CCF voxel coordinates
+        -> master
+        x   :  int   # (um)  Anterior-to-Posterior (AP axis)
+        y   :  int   # (um)  Superior-to-Inferior (DV axis)
+        z   :  int   # (um)  Left-to-Right (ML axis)
+        index(y, z)
+        """
+
+@lab.schema
 class BrainRegion(dj.Manual):
     definition = """
-    -> master
+    -> CCF
     acronym: varchar(32)
     ---
     region_name: varchar(128)
