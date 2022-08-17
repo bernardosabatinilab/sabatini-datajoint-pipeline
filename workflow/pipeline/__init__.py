8e3a82d427504c5133ba0aa2dddc22885f60e58d
Sidharth Hulyalkar <sid.soccer.21@gmail.com>
2022-08-17 14:49:36 -0500

    fixed import mislabel (#17)
    Co-authored-by: Ubuntu <ubuntu@ip-172-20-48-232.us-east-2.compute.internal>

--- workflow/pipeline/__init__.py
+++ workflow/pipeline/__init__.py
@@ -1,6 +1,6 @@
 from .core import lab, subject, session
 from .imaging import scan, imaging
 from .dlc import model, train
-from .events import event, trial
+from .event import event, trial
 from .ephys import ephys, probe
 # from .photometry import *
