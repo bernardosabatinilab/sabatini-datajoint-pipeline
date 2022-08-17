c7190f21388acf05b2f315a1c8ef8a62df46192e
JR <jaerong.ahn@datajoint.com>
2022-08-17 18:37:56 -0500

    🐛 merge changes from the sciops branch

--- workflow/pipeline/imaging.py
+++ workflow/pipeline/imaging.py
@@ -7,7 +7,8 @@ from .core import lab, session
 from workflow.utils.paths import (
     get_imaging_root_data_dir,
     get_imaging_processed_root_data_dir,
-    get_imaging_session_directory,
+    get_nd2_files,
+    get_scan_box_files,
     get_scan_image_files,
 )
 
@@ -17,13 +18,16 @@ __all__ = ["scan", "imaging"]
 Session = session.Session
 Equipment = lab.Equipment
 
+
 @lab.schema
 class BrainRegion(dj.Manual):
+
     definition = """
     acronym: varchar(32)
     ---
     region_name: varchar(128)
     """
+
 
 Location = BrainRegion
 
