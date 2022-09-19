a776f7c0cf8073c8d58004fe91d69c5d326881fe
Jaerong Ahn <jaerong.ahn@datajoint.com>
2022-09-19 16:24:41 -0500

    feat: :sparkles: photometry ingestion routine

--- workflow/pipeline/photometry.py
+++ workflow/pipeline/photometry.py
@@ -4,6 +4,8 @@ from .core import session, lab, subject
 from workflow import db_prefix
 from element_interface.utils import find_full_path
 from workflow.utils.paths import get_raw_root_data_dir
+import typing as T
+import pandas as pd
 
 logger = dj.logger
 schema = dj.schema(db_prefix + "photometry")
@@ -12,33 +14,33 @@ schema = dj.schema(db_prefix + "photometry")
 @schema
 class SensorProtein(dj.Lookup):
     definition = """            
-    name                : varchar(16)  # (e.g., GCaMP, dLight, etc)
+    sensor_protein_name : varchar(16)  # (e.g., GCaMP, dLight, etc)
     ---
     notes=''            : varchar(64)  
     """
 
+
 @schema
 class LightSource(dj.Lookup):
     definition = """
-    name                : varchar(16)
+    light_source_name   : varchar(16)
     """
-    contents = zip(['Plexon LED', 'Laser'])
-    
-    
+    contents = zip(["Plexon LED", "Laser"])
+
+
 @schema
 class ExcitationWavelength(dj.Lookup):
     definition = """
-    excitation_wavelength   : smallint 
+    excitation_wavelength   : smallint (nm)
     """
 
 
 @schema
 class EmissionColor(dj.Lookup):
     definition = """
-    emission_color          : varchar(10) 
+    emission_color     : varchar(10) 
     ---
-    -> LightSource
-    emission_wavelength     : smallint
+    wavelength=null    : smallint (nm)
     """
 
 
@@ -46,10 +48,12 @@ class EmissionColor(dj.Lookup):
 class FiberPhotometry(dj.Imported):
     definition = """
     -> session.Session
-    fiber_id            : int    # id of the implanted fibers
+    fiber_id            : tinyint unsigned    
     ---
+    side                : enum("left", "right")
     sample_rate         : float  # (in Hz) 
     timestamps          : longblob  # (in seconds) photometry timestamps, already synced to the master clock
+    -> [nullable] LightSource
     notes=''            : varchar(1000)  
     """
 
@@ -63,11 +67,11 @@ class FiberPhotometry(dj.Imported):
     class Trace(dj.Part):
         definition = """ # preprocessed photometry traces
         -> master
-        channel_id      : int
+        channel_name    : varchar(8)
         ---
-        -> SensorProtein          
-        -> ExcitationWavelength
         -> EmissionColor
+        -> [nullable] SensorProtein          
+        -> [nullable] ExcitationWavelength
         trace           : longblob
         """
 
@@ -80,45 +84,241 @@ class FiberPhotometry(dj.Imported):
 
     def make(self, key):
 
-        # Meta info hard-coded for now
-        sample_rate = 50
-        time_offset = 67.94
-        excitation_wavelength = 405
-        emission_color = "green"
-        notes = ""
+        import pandas as pd
+        import numpy as np
+        from pathlib import Path
+        import tomli
+        import workflow.utils.photometry_preprocessing as pp
+        from workflow.utils import demodulation
+        import tdt
+        from copy import deepcopy
 
+        # Parameters
+        fiber_to_side_mapping = {1: "right", 2: "left"}
+        color_mapping = {"g": "green", "r": "red", "b": "blue"}
+        behavior_sample_rate = 200  # original behavioral sampling freq (Hz)
+        target_downsample_rate = 50  # (Hz)
+        downsample_factor = behavior_sample_rate / target_downsample_rate
+
+        # Find data dir
+        subject_id = (session.Session & key).fetch1("subject")
         session_dir = (session.SessionDirectory & key).fetch1("session_dir")
-        photometry_dir = (
-            find_full_path(get_raw_root_data_dir(), session_dir) / "photometry"
+        session_full_dir: Path = find_full_path(get_raw_root_data_dir(), session_dir)
+        behavior_dir = session_full_dir / "Behavior"
+        photometry_dir = session_full_dir / "Photometry"
+
+        tdt_data: tdt.StructType = tdt.read_block(photometry_dir)
+
+        photometry_df: pd.DataFrame = demodulation.offline_demodulation(
+            tdt_data, z=True, tau=0.05, downsample_fs=600, bandpass_bw=20
         )
 
-        for fiber in photometry_dir.iterdir():
+        # List working fibers
+        fibers = [
+            i for i in [1, 2] if np.std(tdt_data.streams[f"Fi{i}r"].data[0][5:] > 0.05)
+        ]
+        # del tdt_data
 
-            fiber_id = fiber.stem
+        channels: T.List[str] = photometry_df.columns.drop(
+            ["toBehSys", "fromBehSys"]
+        ).tolist()
 
-            # Populate FiberPhotometry
-            self.insert1([*key.values(), fiber_id, sample_rate, notes])
+        # Update df to start with first trial pulse from behavior system
+        photometry_df = pp.handshake_behav_recording_sys(photometry_df)
 
-            for channel in photometry_dir.rglob("*.csv"):
+        # Resample the photometry data and align to 200 Hz state transition behavioral data (analog_df)
+        behavior_df: pd.DataFrame = pd.read_csv(
+            behavior_dir / f"{subject_id}_behavior_df_full.csv", index_col=0
+        )
+        analog_df: pd.DataFrame = pd.read_csv(
+            behavior_dir / f"{subject_id}_analog_filled.csv", index_col=0
+        )
+        analog_df["session_clock"] = analog_df.index * 0.005
 
-                channel_id = channel.stem[-1]
-                trace = np.genfromtxt(channel, delimiter=",")
-                timestamps = (
-                    np.linspace(0, len(trace), len(trace)) / sample_rate
-                ) + time_offset
+        aligned_behav_photo_df, time_offset = pp.resample_and_align(
+            analog_df, photometry_df, channels=channels
+        )
+        del analog_df
 
-                # Populate FiberPhotometry.Trace
-                self.Trace.insert1(
-                    [
-                        *key.values(),
-                        fiber_id,
-                        channel_id,
-                        excitation_wavelength,
-                        emission_color,
-                        timestamps,
-                        trace,
+        # One more rolling z-score over the window length (60s * sampling freq (200Hz))
+        win = round(60 * 200)
+
+        for channel in channels:
+            if "detrend" in channel:
+                aligned_behav_photo_df[
+                    f'z_{channel.split("_")[-1]}'
+                ] = demodulation.rolling_z(aligned_behav_photo_df[channel], wn=win)
+        aligned_behav_photo_df = aligned_behav_photo_df.iloc[win:-win].reset_index(
+            drop=True
+        )  # drop edges that now contain NaNs from rolling window
+
+        # Drop unnecessary columns that we don't need to save
+        photo_columns = channels + [
+            f'z_{channel.split("_")[-1]}' for channel in channels[::3]
+        ]  # channels[::((len(channels)//2)+1)]]]
+        cols_to_keep = [
+            "nTrial",
+            "iBlock",
+            "Cue",
+            "ENL",
+            "Select",
+            "Consumption",
+            "iSpout",
+            "stateConsumption",
+            "ENLP",
+            "CueP",
+            "nENL",
+            "nCue",
+            "session_clock",
+        ]
+        cols_to_keep.extend(photo_columns)
+
+        timeseries_task_states_df: pd.DataFrame = deepcopy(
+            aligned_behav_photo_df[cols_to_keep]
+        )
+        timeseries_task_states_df["trial_clock"] = (
+            timeseries_task_states_df.groupby("nTrial").cumcount() * 5 / 1000
+        )
+
+        # This has to happen AFTER alignment between photometry and behavior because first ENL triggers sync pulse
+        _split_penalty_states(timeseries_task_states_df, behavior_df, penalty="ENLP")
+        _split_penalty_states(timeseries_task_states_df, behavior_df, penalty="CueP")
+
+        n_bins, remainder = divmod(
+            len(timeseries_task_states_df), downsample_factor
+        )  # get number of bins to downsample into
+        bin_ids = [
+            j for i in range(int(n_bins)) for j in np.repeat(i, downsample_factor)
+        ]  # define ids of bins at downsampling rate [1,1,1,1,2,2,2,2,...]
+        bin_ids.extend(
+            np.repeat(bin_ids[-1] + 1, remainder)
+        )  # tag on incomplete bin at end
+        timeseries_task_states_df[
+            "bin_ids"
+        ] = bin_ids  # new column to label new bin_ids
+
+        downsampled_states_df: pd.DataFrame = deepcopy(timeseries_task_states_df)
+
+        # Apply aggregate function to each column
+        col_fcns = {
+            col: np.max
+            for col in downsampled_states_df.columns
+            if col not in photo_columns
+        }
+        [col_fcns.update({col: np.mean}) for col in photo_columns]
+
+        downsampled_states_df = downsampled_states_df.groupby("bin_ids").agg(col_fcns)
+        downsampled_states_df = downsampled_states_df.reset_index(drop=True)
+        downsampled_states_df = downsampled_states_df.drop(columns=["bin_ids"])
+
+        # Read from the meta_info.toml in the photometry folder if exists
+        meta_info_file = list(photometry_dir.glob("*.toml"))[0]
+        with open(meta_info_file.as_posix()) as f:
+            meta_info: T.Dict = tomli.loads(f.read())
+
+        # Populate FiberPhotometry
+        fiber_photometry: T.List[T.Dict[str, T.Any]] = []
+        trace_names: T.List[str] = list(downsampled_states_df.columns[-6:])
+        trace_list: T.List[T.Dict[str, T.Any]] = []
+
+        for fiber_id in fibers:
+
+            try:
+                light_source = meta_info["Fiber"]["light_source"]
+                fiber_notes = meta_info["Fiber"]["Implantation"][
+                    fiber_to_side_mapping[fiber_id]
+                ]["notes"]
+            except:
+                light_source = fiber_notes = ""
+
+            fiber_photometry.append(
+                {
+                    **key,
+                    "fiber_id": fiber_id,
+                    "side": fiber_to_side_mapping[fiber_id],
+                    "sample_rate": target_downsample_rate,
+                    "timestamps": (
+                        np.linspace(
+                            0, len(downsampled_states_df), len(downsampled_states_df)
+                        )
+                        / target_downsample_rate
+                    )
+                    + time_offset,
+                    "light_source": light_source,
+                    "notes": fiber_notes,
+                }
+            )
+
+            # Populate FiberPhotometry.Trace
+            # ['detrend_grnR', 'detrend_grnL', 'raw_grnR', 'raw_grnL', 'z_grnR', 'z_grnL']
+            fiber_to_side_mapping[fiber_id]
+            for trace_name in trace_names:
+
+                try:
+                    sensor_protein = meta_info["VirusInjection"][
+                        fiber_to_side_mapping[fiber_id]
+                    ]["sensor_protein"]
+                except:
+                    sensor_protein = ""
+
+                emission_color = color_mapping[trace_name.split("_")[1][0]]
+
+                try:
+                    emission_wavelength = meta_info["Fiber"]["emission_wavelength"][
+                        emission_color
                     ]
+                except:
+                    emission_wavelength = ""
+
+                trace_list.append(
+                    {
+                        **key,
+                        "channel_name": trace_name.split("_")[0],
+                        "emission_color": emission_color,
+                        "sensor_protein_name": sensor_protein,
+                        "excitation_wavelength": emission_wavelength,
+                        "trace": downsampled_states_df[trace_name].values,
+                    }
                 )
 
-        # Populate FiberPhotometry.TimeOffset
-        self.TimeOffset.insert1([*key.values(), fiber_id, time_offset])
+        self.insert(fiber_photometry)
+        self.Trace.insert(trace_list)
+        self.TimeOffset.insert1([*key.values(), time_offset])
+
+
+def _split_penalty_states(
+    df: pd.DataFrame, behavior_df: pd.DataFrame, penalty: str = "ENLP"
+) -> None:
+    """Handle penalties. Label preceding states as different from those without penalties"""
+    penalty_trials = df.loc[df[penalty] == 1].nTrial.unique()
+
+    if len(penalty_trials) > 1:
+        penalty_groups = df.loc[df.nTrial.isin(penalty_trials)].groupby(
+            "nTrial", as_index=False
+        )
+
+        mask = penalty_groups.apply(
+            lambda x: x[f"n{penalty[:-1]}"]
+            < behavior_df.loc[behavior_df.nTrial == x.nTrial.iloc[0].squeeze()][
+                f"n_{penalty[:-1]}"
+            ].squeeze()
+        )
+
+    else:
+        mask = (
+            df.loc[df.nTrial.isin(penalty_trials), f"n{penalty[:-1]}"]
+            < behavior_df.loc[behavior_df.nTrial.isin(penalty_trials)][
+                f"n_{penalty[:-1]}"
+            ].squeeze()
+        )
+
+    # Label pre-penalty states as penalties
+    df[f"state_{penalty}"] = 0
+    df.loc[df.nTrial.isin(penalty_trials), f"state_{penalty}"] = (
+        mask.values * df.loc[df.nTrial.isin(penalty_trials), f"{penalty[:-1]}"]
+    )
+
+    # Remove pre-penalty states from true states
+    df.loc[df.nTrial.isin(penalty_trials), f"{penalty[:-1]}"] = (
+        1 - mask.values
+    ) * df.loc[df.nTrial.isin(penalty_trials), f"{penalty[:-1]}"]
