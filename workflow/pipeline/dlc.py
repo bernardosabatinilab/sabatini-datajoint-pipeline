8a1d8646654db0bfb8d7dfe7636b16062f6458f3
Sidharth Hulyalkar <sidharth.hulyalkar@datajoint.com>
2023-01-09 14:00:06 -0600

    removed DLC train schema population, and cleaned up unnecessary imports

--- workflow/pipeline/dlc.py
+++ workflow/pipeline/dlc.py
@@ -2,7 +2,6 @@ from element_deeplabcut import train, model
 from workflow import db_prefix
 from workflow.utils.paths import get_dlc_root_data_dir, get_dlc_processed_data_dir
 from workflow.pipeline import lab, session, reference
-from workflow.pipeline.dlc import ModelVideoSet
 
 import yaml
 import re
@@ -16,6 +15,35 @@ Device = reference.Device
 
 train.activate(db_prefix + "train", linking_module=__name__)
 model.activate(db_prefix + "model", linking_module=__name__)
+
+def ingest_behavior_videos(key, device_id, recording_id=0):
+    rel_path = (session.SessionDirectory & key).fetch1('session_dir')
+    dlc_beh_path = model.get_dlc_root_data_dir()[0] / rel_path / "dlc_behavior_videos"
+    beh_vid_files = [beh_file for beh_file in dlc_beh_path.glob('*.avi') if beh_file.is_file()]
+    vid_recs = []
+    vid_recs_files = []
+    for file_idx, bfile in enumerate(beh_vid_files):
+        vid_recs.append(
+                    dict(
+                        subject=key["subject"],
+                        session_id=key["session_id"],
+                        recording_id=recording_id,
+                        device_id=device_id,
+                    )
+                )
+
+        vid_recs_files.append(
+            dict(
+                subject=key["subject"],
+                session_id=key["session_id"],
+                recording_id=recording_id,
+                file_id=file_idx,
+                file_path="/".join(str(bfile).split("/")[4:]),
+            )
+        )
+
+    model.VideoRecording.insert(vid_recs, skip_duplicates=True)
+    model.VideoRecording.File.insert(vid_recs_files, skip_duplicates=True)
 
 def insert_new_dlc_model(project_path, paramset_idx=None, model_prefix="", model_description=""):
     config_file_path = project_path / "config.yaml"
